8d6fec363a7b52e8250138591a0f968bf07f31f4
OwnDroid <askowndroid@gmail.com>
2014-12-22 02:21:19 +0100

    Revert "[Settings]: recents panel clear all button tweaks"
    This reverts commit a4ff4baaea45a2bb1dbe21710a62f3819cd24ed2.

--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -15,17 +15,13 @@
 */
 package com.android.settings.cyanogenmod;
 
-import android.os.Bundle;
-import android.os.UserHandle;
 import android.content.ContentResolver;
-import android.content.res.Resources;
+import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
 import android.provider.Settings;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -36,23 +32,18 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-	private static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
- 	private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
     private static final String STATUS_BAR_BATTERY_STYLE_HIDDEN = "4";
     private static final String STATUS_BAR_BATTERY_STYLE_TEXT = "6";
 
     private ListPreference mStatusBarBattery;
     private ListPreference mStatusBarBatteryShowPercent;
-	private SwitchPreference mRecentsClearAll;
-	private ListPreference mRecentsClearAllLocation;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.status_bar_settings);
 
-		PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
         mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
@@ -70,19 +61,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mStatusBarBatteryShowPercent.setValue(String.valueOf(batteryShowPercent));
         mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
         mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
-        
-		mRecentsClearAll = (SwitchPreference) prefSet.findPreference(SHOW_CLEAR_ALL_RECENTS);
-		mRecentsClearAll.setChecked(Settings.System.getIntForUser(resolver,
-		Settings.System.SHOW_CLEAR_ALL_RECENTS, 1, UserHandle.USER_CURRENT) == 1);
-		mRecentsClearAll.setOnPreferenceChangeListener(this);
-
-		mRecentsClearAllLocation = (ListPreference) prefSet.findPreference(RECENTS_CLEAR_ALL_LOCATION);
-		int location = Settings.System.getIntForUser(resolver,
-		Settings.System.RECENTS_CLEAR_ALL_LOCATION, 0, UserHandle.USER_CURRENT);
-		mRecentsClearAllLocation.setValue(String.valueOf(location));
-		mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
-		updateRecentsLocation(location);
-
     }
 
     @Override
@@ -105,18 +83,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
             mStatusBarBatteryShowPercent.setSummary(
                     mStatusBarBatteryShowPercent.getEntries()[index]);
             return true;
-        } else if (preference == mRecentsClearAll) {
-			boolean show = (Boolean) objValue;
-			Settings.System.putIntForUser(getActivity().getContentResolver(),
-			Settings.System.SHOW_CLEAR_ALL_RECENTS, show ? 1 : 0, UserHandle.USER_CURRENT);
-			return true;
-		} else if (preference == mRecentsClearAllLocation) {
-			int location = Integer.valueOf((String) objValue);
-			Settings.System.putIntForUser(getActivity().getContentResolver(),
-			Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
-				updateRecentsLocation(location);
-			return true;
-			}
+        }
         return false;
     }
 
@@ -125,22 +92,4 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                 || value.equals(STATUS_BAR_BATTERY_STYLE_HIDDEN));
         mStatusBarBatteryShowPercent.setEnabled(enabled);
     }
-
-	private void updateRecentsLocation(int value) {
-	ContentResolver resolver = getContentResolver();
-		Resources res = getResources();
-		int summary = -1;
-
-	Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, value);
-
-	if (value == 0) {
-		Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 0);
-		summary = R.string.recents_clear_all_location_right;
-	} else if (value == 1) {
-		Settings.System.putInt(resolver, Settings.System.RECENTS_CLEAR_ALL_LOCATION, 1);
-		summary = R.string.recents_clear_all_location_left;
-	}
-	if (mRecentsClearAllLocation != null && summary != -1) {
-		mRecentsClearAllLocation.setSummary(res.getString(summary));
-	}
 }
