68d4989e9f9942612497be97c1c99a12df2c0c0a
OwnDroid <askowndroid@gmail.com>
2014-12-24 02:40:51 +0100

    fixit
    Change-Id: I46d27cd2b13205f75987ddaae5c80efb09c50077

--- src/com/android/settings/search/SearchIndexableResources.java
+++ src/com/android/settings/search/SearchIndexableResources.java
@@ -93,7 +93,7 @@ public final class SearchIndexableResources {
 						Ranking.getRankForClassName(MainSettings.class.getName()),
 						NO_DATA_RES_ID,
 						MainSettings.class.getName(),
-						R.drawable.ic_settings_interface));
+						R.drawable.ic_power_system));
 
         sResMap.put(SimSettings.class.getName(),
                 new SearchIndexableResource(
